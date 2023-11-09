81497ed9ef1ffef9edb8391ba51eeff024794bee
Sanket Sonowal <sanketsonowal@gmail.com>
2023-11-09 18:08:03 +0000

    Testing the save logic p1

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 $(document).ready(()=>{
     var buttonColors = ["red", "blue", "green", "yellow"];
     var gamePattern = [];
+    var correctSeq = 0;
     var userClickedPattern = [];
     var level = 0;
     var title = $("#level-title");
     var flag = false;
     var clicks = $(".btn");
+    const fs = require('fs');
     $(document).keypress(function(){
         if(!flag){
             title.text("Level "+level);
@@ -48,6 +50,8 @@ $(document).ready(()=>{
         if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
             if(userClickedPattern.length === gamePattern.length){
                 setTimeout(function() {
+                    // correctSeq += 1;
+                    console(userClickedPattern[currentLevel], " <:> ", gamePattern[currentLevel]);
                     nextSequence();
                 }, delayInMilliseconds);
             }
@@ -62,6 +66,7 @@ $(document).ready(()=>{
             var audio = new Audio("sounds/wrong.mp3");
             audio.play();
             title.text("Game Over, Press Any Key to Restart");
+            correctSeq = 0;
             startOver();
         }
     }
@@ -69,5 +74,19 @@ $(document).ready(()=>{
         level = 0;
         gamePattern = [];
         flag = false;
+    }
+    function writeRecord(){
+        let player_name = prompt("Please enter your name");
+        let current_datetime = new Date();
+        let score = (level-1)*100
+        let data = {
+            name: player_name,
+            lvl: level,
+            score: score
+        };
+
+        let record = JSON.stringify(data, null, 2)
+
+        fs.writeFile()
     }
 })
